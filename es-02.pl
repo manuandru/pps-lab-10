110ff99c231c78f108702d937dcc55dbaa802266
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-05-08 20:56:36 +0200

    feat(2.4)

--- es-02.pl
+++ es-02.pl
@@ -15,3 +15,26 @@ sum(N, s(M), s(O)) :- sum(N, M, O).
 % sum_list(cons(zero,cons(s(s(zero)),cons(s(zero),nil))), X). yes. → X/s(s(s(zero)))
 sum_list(nil, zero).
 sum_list(cons(H, T), O) :- sum_list(T, PS), sum(H, PS, O).
+
+
+
+% count(List, Element, NOccurrences)
+% it uses count(List, Element, NOccurrencesSoFar, NOccurrences)
+count(L, E, N) :- count(L, E, zero, N).
+count(nil, E, N, N).
+count(cons(H, T), E, N, M) :- count(T, E, s(N), M).
+count(cons(H, T), E, N, M) :- H \= E, count(T, E, N, M).
+
+
+
+greater(N, zero).
+greater(s(N), s(M)) :- greater(N, M).
+% max(List , Max)
+% Max is the biggest element in List
+% Suppose the list has at least one element
+max(cons(H, T), M) :- max(T, H, M).
+max(nil, TM, TM).
+max(cons(H, T), TM, M) :- greater(H, TM), max(T, H, M).
+max(cons(H, T), TM, M) :- greater(TM, H), max(T, TM, M).
+
+% max(cons(s(zero), cons(s(s(zero)), cons(zero, cons(s(s(s(zero))), nil)))), X). yes. → X / s(s(s(s(zero))))
