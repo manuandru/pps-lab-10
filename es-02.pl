946f06c5d1704b889ac813111b546ea751a1d78a
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-05-08 19:23:32 +0200

    feat(2.2)

--- es-02.pl
+++ es-02.pl
@@ -5,3 +5,13 @@ size(cons(_, T), s(N)) :- size(T, N).
 
 % size(cons(a,cons(b,cons(c,nil))), X). → X/s(s(s(zero)))
 % size(L, s(s(s(zero)))).
+
+
+% sum(A, B, A+B).
+sum(N, zero, N).
+sum(N, s(M), s(O)) :- sum(N, M, O).
+
+% sum_list(List, Sum) 2
+% sum_list(cons(zero,cons(s(s(zero)),cons(s(zero),nil))), X). yes. → X/s(s(s(zero)))
+sum_list(nil, zero).
+sum_list(cons(H, T), O) :- sum_list(T, PS), sum(H, PS, O).
