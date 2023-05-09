548540372bcf7d68502c05daecf70368d683729c
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-05-09 18:43:01 +0200

    feat(3.2)

--- es-03.pl
+++ es-03.pl
@@ -1,3 +1,13 @@
 % same(List1, List2)
 % are the two lists exactly the same?
 same(L, L).
+
+
+
+greater(N, zero).
+greater(s(N), s(M)) :- greater(N, M).
+% all_bigger(List1,List2)
+% all elements in List1 are bigger than those in List2, 1 by 1
+% all_bigger(cons(s(s(zero)), cons(s(zero), nil)),cons(s( zero),cons(zero, nil))).
+all_bigger(nil, nil).
+all_bigger(cons(H1, T1), cons(H2, T2)) :- greater(H1, H2), all_bigger(T1, T2).
