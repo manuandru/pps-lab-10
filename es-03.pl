d79a7c106d8b1ba9ffe0459e1fe516cfebe094b3
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-05-09 18:48:17 +0200

    feat(3.3)

--- es-03.pl
+++ es-03.pl
@@ -4,10 +4,20 @@ same(L, L).
 
 
 
-greater(N, zero).
+greater(_, zero).
 greater(s(N), s(M)) :- greater(N, M).
 % all_bigger(List1,List2)
 % all elements in List1 are bigger than those in List2, 1 by 1
 % all_bigger(cons(s(s(zero)), cons(s(zero), nil)),cons(s( zero),cons(zero, nil))).
 all_bigger(nil, nil).
 all_bigger(cons(H1, T1), cons(H2, T2)) :- greater(H1, H2), all_bigger(T1, T2).
+
+
+
+search(X, cons(X, _)).
+search(X, cons(_, T)) :- search(X, T).
+% sublist(List1 ,List2)
+% List1 should contain elements all also in List2
+% sublist(cons(a,cons(b,nil)), cons(c,cons(b,cons(a,nil)))).
+sublist(nil, _).
+sublist(cons(H, T), L) :- search(H, L), sublist(T, L).
