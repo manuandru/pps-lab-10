e0e1ca64b41a61fe5650e485695da3aaad5732fe
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-05-10 11:26:07 +0200

    feat(4.3)

--- es-04.pl
+++ es-04.pl
@@ -13,3 +13,15 @@ seq(s(N), E, cons(E,T)) :- seq(N, E, T).
 % seqR(s(s(s(zero))), cons(s(s(zero)),cons(s(zero),cons(zero,nil)))).
 seqR(zero, nil).
 seqR(s(N), cons(H, T)) :- N = H, seqR(N, T).
+
+
+% last(List, e, List :: e)
+% last(cons(a,cons(b,nil)),c,cons(a,cons(b,cons(c,nil)))).
+last(nil, E, cons(E, nil)).
+last(cons(H, T), E, cons(H, O)) :- last(T, E, O).
+
+% seqR2(N,List) --> is [0,1,...,N-1]
+% seqR2(s(s(s(zero))), cons(zero,cons(s(zero),cons(s(s(zero) ),nil)))).
+seqR2(N, L) :- seqR2(N, nil, L).
+seqR2(zero, L, L).
+seqR2(s(N), L, LN) :- seqR2(N, L, O), last(O, N, LN).
