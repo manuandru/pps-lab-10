7f4d3a39679f0cfe67f6fca7f6db2c8a31161006
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-05-10 14:33:40 +0200

    feat(5.1): map

--- es-05.pl
+++ es-05.pl
@@ -2,3 +2,10 @@
 % last(cons(zero, cons(zero, cons(s(zero), nil))), s(zero)).
 last(cons(H, nil), H).
 last(cons(_, T), O) :- last(T, O).
+
+
+
+% map(ListIn, ListOut): ListOut[i] = ListIn[i] + 1
+% map(cons(zero, cons(s(zero), nil)), X). yes -> X / cons(s(zero),cons(s(s(zero)),nil))
+map(nil, nil).
+map(cons(H1, T1), cons(s(H1), T2)) :- map(T1, T2).
