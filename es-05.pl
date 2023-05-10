d1b55dc2d411d768347a53cc8235ccbeea4fde1b
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-05-10 15:03:21 +0200

    feat(5.1): filter

--- es-05.pl
+++ es-05.pl
@@ -9,3 +9,12 @@ last(cons(_, T), O) :- last(T, O).
 % map(cons(zero, cons(s(zero), nil)), X). yes -> X / cons(s(zero),cons(s(s(zero)),nil))
 map(nil, nil).
 map(cons(H1, T1), cons(s(H1), T2)) :- map(T1, T2).
+
+
+greater(s(_), zero).
+greater(s(N), s(M)) :- greater(N, M).
+% filter(ListIn, ListOut): ListOut = ListIn \ {x: x <= 0}
+% filter(cons(zero, cons(s(sero), cons(zero, nil))),cons(s(sero), nil)).
+filter(nil, nil).
+filter(cons(H1, T1), cons(H1, T2)) :- greater(H1, zero), filter(T1, T2).
+filter(cons(H1, T1), T2) :- H1 = zero, filter(T1, T2).
