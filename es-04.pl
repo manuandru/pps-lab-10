9c437aface1b57efe66fd6d92f98073a4f486950
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-05-10 10:48:49 +0200

    feat(4.2)

--- es-04.pl
+++ es-04.pl
@@ -6,3 +6,10 @@ seq(s(N), E, cons(E,T)) :- seq(N, E, T).
 % fully relational
 % seq(X, Y, cons(a, cons(a, nil))). -> X / s(s(zero)), Y / a
 % seq(s(s(s(zero))), a, L). -> L / cons(a,cons(a,cons(a,nil)))
+
+
+
+% seqR(N, List)
+% seqR(s(s(s(zero))), cons(s(s(zero)),cons(s(zero),cons(zero,nil)))).
+seqR(zero, nil).
+seqR(s(N), cons(H, T)) :- N = H, seqR(N, T).
